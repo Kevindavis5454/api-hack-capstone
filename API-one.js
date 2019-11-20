9b4bef79c17f27ab6dc88e16647e552873b47d64
Kevin Davis <Kevindavis5454@gmail.com>
2019-11-20 10:40:51 -0700

    Added Trefle API

--- API-one.js
+++ API-one.js
@@ -2,9 +2,15 @@
 
 //Trefle
 
-const searchURL = 'https://trefle.io/api/plants';
+const searchURLOne = 'https://trefle.io/api/plants';
 
-const apiKey = "eGxMYTdpWXQ1eUh2T1FDMy95RHZNQT09";
+const searchURLId = 'https://trefle.io/api/plants/';
+
+const apiKeyOne = "eGxMYTdpWXQ1eUh2T1FDMy95RHZNQT09";
+
+const linkKey = '?token=eGxMYTdpWXQ1eUh2T1FDMy95RHZNQT09';
+
+
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -13,29 +19,23 @@ function formatQueryParams(params) {
 }
 
 
-function getTrefle(query, maxResults) {
-    //let state = $("#state-option-one option:selected").map(function(){return this.text}).get().join(',');
+function getTrefle(query) {
 
     const params = {
-        //token : 'eGxMYTdpWXQ1eUh2T1FDMy95RHZNQT09',
+        token : apiKeyOne,
         q: query,
-        //limit: maxResults,
-        //stateCode: state,
     };
 
     const queryString = formatQueryParams(params);
 
-    const url = searchURL + '?' + queryString;
+    const url = searchURLOne + '?' + queryString;
 
     console.log(url);
 
-    const options = {
-        headers: new Headers({
-            "Authorization" : "Bearer" + apiKey,
-        })
-    };
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+        targetUrl = url;
 
-    fetch(url, options)
+    fetch(proxyUrl + targetUrl)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -50,36 +50,109 @@ function getTrefle(query, maxResults) {
 
 function displayResults(responseJson) {
     console.log(responseJson);
+
     $('#results-list-one').empty();
 
-    for (let i = 0; i < responseJson.data.length; i++){
+    for (let i = 0; i < responseJson.length; i++){
 
         $('#results-list-one').append(`
-        <li><h3>${responseJson.data[i].fullName}</h3><h4>${responseJson.data[i].states}</h4>
-        <p>${responseJson.data[i].description}</p>
-        <a href="${responseJson.data[i].url}">Website</a>
-        <p>${responseJson.data[i].directionsInfo}  <a href="${responseJson.data[i].directionsURL}">Directions</a></p>
+        <li><h3>${responseJson[i].common_name}</h3>
+        <p>${responseJson[i].scientific_name}</p>
+        <p>Plant Id: ${responseJson[i].id}</p>
+        </li>
             
         `)};
-    $('#results').removeClass('hidden');
+    $('#results-one').removeClass('hidden');
 };
 
 
 
+//Function to format the query Parameters for the Plant ID API query
+function formatQueryParamsIdSearch(paramsIdSearch) {
+    const queryItems = Object.keys(paramsIdSearch)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(paramsIdSearch[key])}`)
+    return queryItems.join('&');
+}
 
 
 
+//function to query the Trefle API off the plant ID and return Json Data
+function getPlantIdSearch(queryId) {
 
-function watchForm() {
-    $('form').submit(event => {
+    const paramsIdSearch = {
+        token : apiKeyOne,
+    };
+
+    const queryStringIdSearch = formatQueryParamsIdSearch(paramsIdSearch);
+
+    const urlIdSearch = searchURLId + queryId + '?' + queryStringIdSearch;
+
+    console.log(urlIdSearch);
+
+    const proxyUrlIdSearch = 'https://cors-anywhere.herokuapp.com/',
+        targetUrlIdSearch = urlIdSearch;
+
+    fetch(proxyUrlIdSearch + targetUrlIdSearch)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResultsPlantId(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+
+}
+
+
+// Function to display the Results from the Plant ID Search of the Trefle API
+function displayResultsPlantId (responseJson) {
+    console.log(responseJson);
+    $('#results-list-one').empty();
+
+    $('#results-list-one').append(`
+        <li><h3>Common Name: ${responseJson.common_name}</h3>
+            <h4>Scientific Name: ${responseJson.scientific_name}</h4>
+            <img src="${responseJson.images[0].url}" alt="Flower Picture">
+            <img src="${responseJson.images[1].url}" alt="Flower Picture">
+       </li>
+       <li><h3>Growing Information</h3>
+       <p>Temperature Minimum:</p>
+       </li>
+        
+            
+        `);
+
+}
+
+
+
+//Function for the user to get specific plant information from the Trefle API using the plant ID
+function watchPlantIdSearch() {
+    $('#js-form-id-search').submit(event => {
         event.preventDefault();
-        const searchTermOne = $('#js-search-term-one').val();
-        //const maxResults = $('#js-max-results').val();
-        getTrefle(searchTermOne); // maxResults);
+        const searchIdOne = $('#js-plant-id').val();
+        getPlantIdSearch(searchIdOne);
     });
 }
 
+
+
+//Function to initially get Trefle Api Information from a common/scientific name search
+function watchPlantSearch() {
+    $('#js-form').submit(event => {
+        event.preventDefault();
+        const searchTermOne = $('#js-search-term-one').val();
+        getTrefle(searchTermOne);
+    });
+}
+
+
+//Function to Start Jquery on the Page
 $(function() {
     console.log('App loaded! Waiting for submit');
-    watchForm();
+    watchPlantSearch();
+    watchPlantIdSearch();
 });
